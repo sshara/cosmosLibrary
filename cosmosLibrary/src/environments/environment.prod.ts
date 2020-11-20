eb7d3f348e6c35938130bf65076e57dc20534b7f
Edwin <ed.galeano1@gmail.com>
2020-11-20 18:38:45 -0500

    firbase

--- cosmosLibrary/src/environments/environment.prod.ts
+++ cosmosLibrary/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBytKWaDEYsd5z2etErIMi21Hzf3MuyknU",
+    authDomain: "cosmos-library.firebaseapp.com",
+    databaseURL: "https://cosmos-library.firebaseio.com",
+    projectId: "cosmos-library",
+    storageBucket: "cosmos-library.appspot.com",
+    messagingSenderId: "582380640445",
+  }
 };
