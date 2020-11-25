1b8bfb0bdeb940567e1e4947bab7b88b7b784770
Sara Rincón <sshara.16@gmail.com>
2020-11-25 01:07:31 -0500

    signup, profile

--- cosmosLibrary/src/app/components/client/profile/profile.component.ts
+++ cosmosLibrary/src/app/components/client/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { GeneralService } from 'src/app/services/system/general.service';
+import { PasswordMatchVaildator } from 'src/app/validators/password-match.validator';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  countryOptions: any[];
+  stateOptions: any[];
+  cityOptions: any[];
+  genderOptions: any[];
+  topicOptions: any[];
+
+  updateprofileForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+    lastname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+    birthdate: new FormControl('', [Validators.required]),
+    country: new FormControl('',[Validators.required]),
+    state: new FormControl('',[Validators.required]),
+    city: new FormControl('',[Validators.required]),
+    gender: new FormControl('',[Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+    topic: new FormControl('',[Validators.required]),
+    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+    oldPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmationPassword: new FormControl('', [Validators.required, Validators.minLength(8)])
+  }, {validators: this._passwordMatchValidator.validate})
+
+  constructor(private _generalService:GeneralService,
+              private _passwordMatchValidator: PasswordMatchVaildator) 
+              { 
+                this.countryOptions = [];
+                this.stateOptions = [];
+                this.cityOptions = [];
+                this.genderOptions = [];
+                this.topicOptions = [{id: 'literatura', name: 'literatura'}, { id: 'ES', name: 'español'}, { id: 'FR', name: 'francaise'} ];
+              }
 
   ngOnInit(): void {
+  }
+
+  updateProfile(){
+
+  }
+
+  goTo(route:string){
+    this._generalService.goTo(route);
+  }
+
+  isFormError(){
+    const {errors} = this.updateprofileForm;
+    const passwordTouched = this.updateprofileForm.get('password').touched;
+    const confirmPasswordTouched = this.updateprofileForm.get('confirmationPassword').touched;
+    return errors && passwordTouched && confirmPasswordTouched;
   }
 
 }
