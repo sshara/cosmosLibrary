4bbeb56bae0a007616560be259545fa1d8f81e12
Heckthor Marin <heckthor.marin.71@gmail.com>
2020-11-25 01:00:43 -0500

    password recover

--- cosmosLibrary/src/app/services/session.service.ts
+++ cosmosLibrary/src/app/services/session.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+import { MailService } from './system/mail.service';
+import { GeneralService } from './system/general.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,9 @@ export class SessionService {
   userRef: AngularFireObject<any>;
 
   constructor(
-    private firebase: AngularFireDatabase
+    private firebase: AngularFireDatabase,
+    private _mailService: MailService,
+    private _generalService:GeneralService
   ) { }
 
   logIn(username:string){
@@ -18,10 +22,39 @@ export class SessionService {
     return this.userRef.valueChanges();
   }
 
-  signUp(user:any){
-    let {username} = user;
-    user.role = 'client';
-    this.userRef = this.firebase.object(`users/${username}`);
-    return this.userRef.set(user);
+  signUp(data:any){
+    let {username, email} = data;
+    this.usersRef = this.firebase.list('/users', ref => ref.orderByChild('email').equalTo(email));
+    let subscriptor = this.usersRef.valueChanges().subscribe(users =>{
+      let user = users.pop();
+      if(user){
+        this._generalService.openSnackBar({message:'El correo electrónico ingresado, ya se encuentra registrado.'});
+      }else{
+        data.role = 'client';
+        this.userRef = this.firebase.object(`users/${username}`);
+        return this.userRef.set(user);
+      }
+      subscriptor.unsubscribe();
+    })
+
+    
+  }
+
+  recoverPassword(data:any){
+    data.code = 'a7huJlz4mW';
+    let {email,code} = data;
+    this.usersRef = this.firebase.list('/users', ref => ref.orderByChild('email').equalTo(email));
+    let subscriptor = this.usersRef.valueChanges().subscribe(users =>{
+      let user = users.pop();
+      if(user){
+        let { username } = user;
+        this.firebase.object(`users/${username}`).update({password:code});
+        this._mailService.sendEmail({...data, ...user});
+      }else{
+        this._generalService.openSnackBar({message:'El correo electrónico ingresado, no pertenece a ninguna cuenta.'})
+      }
+      subscriptor.unsubscribe();
+    })
+    
   }
 }
