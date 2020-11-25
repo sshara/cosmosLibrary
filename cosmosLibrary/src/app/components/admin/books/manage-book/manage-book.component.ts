65b0749d7ea2d5514ae5c3907e4a017d2864e76a
Edwin <ed.galeano1@gmail.com>
2020-11-25 01:12:00 -0500

    greo

--- cosmosLibrary/src/app/components/admin/books/manage-book/manage-book.component.ts
+++ cosmosLibrary/src/app/components/admin/books/manage-book/manage-book.component.ts
@@ -7,7 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageBookComponent implements OnInit {
 
-  constructor() { }
+  public books:any[];
+
+  constructor() { 
+    this.books = [
+      
+      
+      {
+        front_image:'https://compote.slate.com/images/73f0857e-2a1a-4fea-b97a-bd4c241c01f5.jpg', 
+        title: 'Patata',
+        author:'Jonh Wick',
+        language:'Francés',
+        topic:'Drama',
+        editorial:'Los enanitos azules'
+      },
+      {
+        front_image:'https://i.pinimg.com/736x/e3/32/3f/e3323fc80a203239e2a28ae23f83260a.jpg', 
+        title: 'Patata',
+        author:'Jonh Wick',
+        language:'Francés',
+        topic:'Drama',
+        editorial:'Los enanitos azules'
+      },
+      {
+        front_image:'https://i0.wp.com/bestlifeonline.com/wp-content/uploads/2019/03/baby-tiger.jpg', 
+        title: 'Patata',
+        author:'Jonh Wick',
+        language:'Francés',
+        topic:'Drama',
+        editorial:'Los enanitos azules'
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
