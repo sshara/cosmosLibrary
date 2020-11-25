668967a682a8578b9e0807ab71f6c69093654884
Heckthor Marin <heckthor.marin.71@gmail.com>
2020-11-25 08:46:15 -0500

    admin

--- cosmosLibrary/src/app/services/admin.service.ts
+++ cosmosLibrary/src/app/services/admin.service.ts
@@ -11,6 +11,8 @@ export class AdminService {
 
   booksRef: AngularFireList<any>;
   bookRef: AngularFireObject<any>;
+  usersRef: AngularFireList<any>;
+  userRef: AngularFireObject<any>;
 
   constructor(
     private firebase: AngularFireDatabase,
@@ -118,7 +120,7 @@ export class AdminService {
     .then(res => {
       this._generalService.openSnackBar({message:'Se ha actualizado el libro satisfactoriamente.'});
       this.uploadFiles({isbn, front_image, back_image});
-      this._generalService.goTo('main-admin');
+      this._generalService.goTo('manage-books');
     })
     .catch(err=>{
       this._generalService.openSnackBar({message:'Ocurrió un error al actualizar el libro.'});
@@ -147,5 +149,41 @@ export class AdminService {
     .catch(err=>{
       this._generalService.openSnackBar({message:'Ocurrió un error al eliminar el libro.'})
     })
+  }
+
+  getAdminData(){
+    let { username } = this._generalService.loadInfo('identity');
+    console.log(username);
+    this.userRef = this.firebase.object(`users/${username}`);
+    return this.userRef.valueChanges();
+  }
+
+  updateAdminData(data:any){
+    let { username } = this._generalService.loadInfo('identity');
+    data.birthdate = data.birthdate.toString();
+
+    data = this._generalService.deleteUnchanged(data);
+    
+    if(data.password !== data.confirmationPassword || !data.password || !data.confirmationPassword){
+      data = this._generalService.deletePasswords(data);
+    }
+    let subscriptor = this.firebase.object(`users/${username}`).valueChanges().subscribe((user:any) => {
+      if(user.password !== data.oldPassword && data.oldPassword){
+        this._generalService.openSnackBar({message:'Contraseña erronea.'});
+      }else{
+        this.userRef = this.firebase.object(`users/${username}`);
+        this.userRef.update(data)
+        .then(update => {
+          this._generalService.openSnackBar({message:'Se ha actualizado la cuenta satisfactoriamente.'});
+          this._generalService.goTo('main-admin');
+        })
+        .catch(err=>{
+          this._generalService.openSnackBar({message:'Ocurrió un error al actualizar la cuenta.'});
+        });
+      }
+      subscriptor.unsubscribe();
+    })
+    
+    
   }
 }
